6ca28394cc8641d96712c29498015569a22a7116
jsbuitragoc53@gmail.com <jsbuitragoc53@gmail.com>
2025-10-02 21:47:26 -0500

    Segundo corte mmm primer commit yo que se

---
+++ BestFitMemorySlotManager.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ur_os.memory.freememorymagament;
+
+/**
+ *
+ * @author super
+ */
+public class BestFitMemorySlotManager extends FreeMemorySlotManager{
+    
+    public BestFitMemorySlotManager(int memSize){
+        super(memSize);
+    }
+    
+    @Override
+    public MemorySlot getSlot(int size) {
+        MemorySlot m = null;
+        //ToDo
+        
+        return m;
+    }
+    
+}
---
+++ WorstFitMemorySlotManager.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ur_os.memory.freememorymagament;
+
+/**
+ *
+ * @author super
+ */
+public class WorstFitMemorySlotManager extends FreeMemorySlotManager{
+    
+    public WorstFitMemorySlotManager(int memSize){
+        super(memSize);
+    }
+    
+    @Override
+    public MemorySlot getSlot(int size) {
+        MemorySlot m = null;
+        //ToDo
+        
+        return m;
+    }
+    
+}
